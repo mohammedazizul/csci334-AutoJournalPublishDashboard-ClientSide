60228a7f22a92afd37742d61e1cb99c6e0e2ebcd
Aozone <wufuyunozone@gmail.com>
2021-10-09 23:11:41 +0800

    Review.js updated and add a new Table.css file under /src/components/Dashboard/Pages/Review/

--- src/components/Dashboard/Pages/Review/Review.js
+++ src/components/Dashboard/Pages/Review/Review.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Table.css";
 
 const Review = () => {
   return (
@@ -13,6 +14,50 @@ const Review = () => {
       }}
     >
       <h1>REVIEW</h1>
+
+      <form>
+        <table>
+          <thead>
+            <tr>
+              <th><input type="checkbox"></input></th>
+              <th>No.</th>
+              <th>Title</th>
+              <th>Topic</th>
+              <th>Pages</th>
+              <th>Submit Date</th>
+              <th>Author Name</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="checkbox"></input></td>
+              <td></td>
+              <td>Preschool Education</td>
+              <td>Education</td>
+              <td>367</td>
+              <td>21/9/2021</td>
+              <td>Tomas John</td>
+              <td>New</td>
+              <td>View</td>
+            </tr>
+            <tr>
+              <td><input type="checkbox"></input></td>
+              <td></td>
+              <td>Kinematics and Health</td>
+              <td>Health</td>
+              <td>567</td>
+              <td>13/11/2019</td>
+              <td>James Lee</td>
+              <td>New</td>
+              <td>View</td>
+            </tr>
+          </tbody>
+        </table>
+        <input type="submit" value="Within Scope"></input>
+        <input type="submit" value="Out of Scope"></input>
+      </form>
     </div>
   );
 };
