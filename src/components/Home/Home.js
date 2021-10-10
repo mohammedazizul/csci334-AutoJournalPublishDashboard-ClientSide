d8565da8ab8c71fc40d538073d89b44c0eff6bde
Tiamo_SUN <19981201sunchao@gmail.com>
2021-10-10 12:17:57 +0800

    Added background & logo for home page-Sam

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Home.css";
-import Logo from "../Logo/logo192.png";
+import Logo from "../Logo/logo512.png";
 
 const Home = () => {
   return (
     <div className="homeMainDiv" id="bgsetting">
-        <div className="innerDiv">
           <img src={Logo} alt="logo"/>
-        </div>
     </div>
   );
 };
