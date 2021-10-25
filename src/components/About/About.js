6a0cc9f35e00d27dec006a7b0beb6c599fb52280
mohammedazizul <mohammedazizulhoquesheikh@gmail.com>
2021-10-25 10:46:33 +0800

    GET and POST request sample updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const About = () => {
-  // GET -> to read
-  // POST -> to send
-  // PATCH -> to update
-  // DELETE ->
-
   // STANDARD GET REQUEST
   const userDataUrl =
-    "http://localhost/api/read/getperson.php?api_key=RXru1LUOOeKFX03LGSo7";
+    "http://localhost/jess-backend/api/read/getperson.php?api_key=RXru1LUOOeKFX03LGSo7";
   const [userDataLoading, setUserDataLoading] = useState(true);
   const [userData, setUserData] = useState([]);
 
@@ -33,38 +28,35 @@ const About = () => {
       });
   }, []);
 
-  const userObj = {
-    dob: "2000-01-01",
-    email: "ryan@example.com",
-    password: "5f4dcc3b5aa765d61d8327deb882cf99",
-    personID: "P00010",
-    type: 0,
-    username: "ryan",
-  };
+  // STANDARD POST REQUEST
+  // POST - (WORKING FINE)
 
-  // POST - (NEED TO TEST)
-  // const sendData = () => {
-  //   const urlToPost = `http://localhost/api/create/createperson.php`;
-  //   fetch(urlToPost, {
-  //     method: "POST",
-  //     // mode: "no-cors",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(userObj),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log("Data: ", data);
-  //       setUserDataLoading(false);
-  //       setUserData(data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error: ", error);
-  //     });
-  // };
-  // sendData();
+  const [inputText, setInputText] = useState("");
+  let formData = new FormData();
+  formData.append("text", inputText); // "text" should be in the back end
+
+  // to Display the key/value pairs
+  // for (var pair of formData.entries()) {
+  //   console.log("Form Data: ", pair[0] + ", " + pair[1]);
+  // }
+
+  const sendData = () => {
+    const urlToPost = `http://localhost/jess-backend/api/create/createperson.php`;
+
+    fetch(urlToPost, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: formData,
+    })
+      .then((response) => console.log("BE Response: ", response))
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
 
   return (
     <div style={{ textAlign: "center" }}>
@@ -84,8 +76,32 @@ const About = () => {
         )}
       </div>
       {/* start after this */}
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button onClick={sendData}>Click To Insert</button>
     </div>
   );
 };
 
 export default About;
+
+// POST BACKEND CODE FOR PHP
+// <?php
+//     require_once '../../connection.php';
+
+//     $recText = $_POST['text'];
+//     echo $_POST;
+
+//     $query = "INSERT INTO react_php (`id`, `text`)  VALUES (NULL, '$recText')";
+
+//         if ($connection->query($query) === TRUE) {
+//             echo "New record created successfully";
+//         } else {
+//             echo "Error: " . $query . "<br>" . $connection->error;
+//         }
+//         $connection->close();
+// ?>
